7ae2c609a00e42dc250ea9fae5e60faaf6ade508
Akshay Sonvane <akshay.sonvane93@gmail.com>
2017-02-16 15:22:38 -0800

    Scrap additional data

--- RESTscraper.py
+++ RESTscraper.py
@@ -8,9 +8,21 @@ json_response = json.loads(get_response.content)
 names = ""
 for data in json_response['guests']:
     names += data['profile']['name'] + "\n"
-    print data['profile']['name']
+    print "Name: " + data['profile']['name']
+    print "Description: " + data['profile'].get('description', 'Null')
+    print "Location: " + data['profile'].get('location', 'Null')
+    if data['service'] == "email":
+        print "Registerd via: Email"
+    elif data['service'] == "facebook":
+        print "Profile: " + "https://www.facebook.com/" + data['profile']['handles']['facebook']
+    elif data['service'] == "linkedin":
+        print "Profile: " + "https://www.linkedin.com/in/" + data['profile']['handles']['linkedin']
+    elif data['service'] == "twitter":
+        print "Profile: " + "https://twitter.com/" + data['profile']['handles']['twitter']
 
-#To remove the last character i.e. "\n" in this case
+    print "\n"
+
+# To remove the last character i.e. "\n" in this case
 names = names[:-1]
 
 outfile = open('output.txt', 'w')
